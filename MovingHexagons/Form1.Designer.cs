ded5fb742dac964910d3401da44611daf5cd2356
Ulas Cosar Coskun <uccoskun@yahoo.com>
2024-01-06 00:13:32 -0600

    transparent panels are used

--- MovingHexagons/Form1.Designer.cs
+++ MovingHexagons/Form1.Designer.cs
@@ -29,23 +29,82 @@ namespace MovingHexagons
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new MovingHexagons.TransparentPanel();
+            this.panel2 = new MovingHexagons.TransparentPanel();
+            this.panel3 = new MovingHexagons.TransparentPanel();
+            this.panel4 = new MovingHexagons.TransparentPanel();
+            this.panel5 = new MovingHexagons.TransparentPanel();
+            this.panel6 = new MovingHexagons.TransparentPanel();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(776, 538);
+            this.panel1.Size = new System.Drawing.Size(210, 184);
             this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(12, 252);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(210, 184);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(228, 252);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(210, 184);
+            this.panel3.TabIndex = 1;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(470, 239);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(210, 184);
+            this.panel4.TabIndex = 1;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(240, 32);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(210, 184);
+            this.panel5.TabIndex = 1;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(470, 32);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(210, 184);
+            this.panel6.TabIndex = 1;
+            this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.ClientSize = new System.Drawing.Size(578, 544);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -54,8 +113,13 @@ namespace MovingHexagons
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer2;
+        private TransparentPanel panel1;
+        private TransparentPanel panel2;
+        private TransparentPanel panel3;
+        private TransparentPanel panel4;
+        private TransparentPanel panel5;
+        private TransparentPanel panel6;
     }
 }
 
--- MovingHexagons/Form1.cs
+++ MovingHexagons/Form1.cs
@@ -15,6 +15,16 @@ namespace MovingHexagons
         public Form1()
         {
             InitializeComponent();
+            panels = new List<Panel>();
+            panels.Add(panel1);
+            panels.Add(panel2);
+            panels.Add(panel3);
+            panels.Add(panel4);
+            panels.Add(panel5);
+            panels.Add(panel6);
+            setPanels();
+            movePanels();
+
 
 
 
@@ -22,26 +32,27 @@ namespace MovingHexagons
 
         }
 
-        System.Timers.Timer timer1;
-        int radius = 100;
+        List<Panel> panels;
+        int radius = 40;
+        int maxRadius = 80;
         int hexRadius = 40;
         bool increase = false;
 
-        private Point rotate(Point center, Point p, int angle)
+        private PointF rotate(PointF center, PointF p, int angle)
         {
             double angleRad = angle * Math.PI / 180;
-            int xcenter = center.X, ycenter = center.Y;
-            int x = p.X - xcenter, y = p.Y - ycenter;
-            int newX, newY;
-            newX = (int)(x * Math.Cos(angleRad) - y * Math.Sin(angleRad) + xcenter);
-            newY = (int)(x * Math.Sin(angleRad) + y * Math.Cos(angleRad) + ycenter);
-            return new Point(newX, newY);
+            float xcenter = center.X, ycenter = center.Y;
+            float x = p.X - xcenter, y = p.Y - ycenter;
+            float newX, newY;
+            newX = (float)(x * Math.Cos(angleRad) - y * Math.Sin(angleRad) + xcenter);
+            newY = (float)(x * Math.Sin(angleRad) + y * Math.Cos(angleRad) + ycenter);
+            return new PointF(newX, newY);
         }
 
-        private void drawHexagons(Graphics g, Point intPoint, Point center)
+        private void drawHexagons(Graphics g, PointF intPoint, PointF center)
         {
             Pen drawingPen = new Pen(Color.Black, 2);
-            Point nextPoint;
+            PointF nextPoint;
             for (int i = 0; i < 6; i++)
             {
                 nextPoint = rotate(center, intPoint, 60);
@@ -50,43 +61,84 @@ namespace MovingHexagons
             }
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void panel_Paint(object sender, PaintEventArgs e)
         {
-            int height = panel1.Height;
-            int width = panel1.Width;
-
+            int height = (sender as Panel).Height;
+            int width = (sender as Panel).Width;
             Graphics g = e.Graphics;
-            Point centerHex = new Point(width / 2 + radius, height / 2);
-            Point intPoint = new Point(centerHex.X + hexRadius, centerHex.Y);
-            Point centerPanel = new Point(width / 2, height / 2);
+            
+            PointF centerPanel = new PointF(width / 2, height / 2);
+            PointF intPoint = new PointF(0, hexRadius);
+            
             for (int i = 0; i < 6; i++)
             {
-                drawHexagons(g, intPoint, centerHex);
-                centerHex = rotate(centerPanel, centerHex, 60);
-                intPoint = new Point(centerHex.X + hexRadius, centerHex.Y);
+                drawHexagons(g, intPoint, centerPanel);
+                centerPanel = rotate(centerPanel, intPoint, 60);
+                intPoint = new PointF(centerPanel.X, centerPanel.Y);
             }
+            (sender as Panel).BackColor = Color.Transparent;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            timer1 = new System.Timers.Timer();
-            timer1.Interval = 300;
-            timer1.Elapsed += Timer1_Elapsed;
-            timer1.Start();
+
+            //timer1 = new System.Timers.Timer();
+            //timer1.Interval = 1000;
+            //timer1.Elapsed +=  moveHexagons;
+            //timer1.Start();
+         //   setPanels();
+
 
         }
 
-        private void Timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+
+        private void timer2_Tick(object sender, EventArgs e)
         {
             if (increase)
                 radius += 1;
             else radius -= 1;
-            if (radius > 200)
+            if (radius > maxRadius)
                 increase = false;
-            else if (radius < hexRadius)
+            else if (radius <= hexRadius)
                 increase = true;
-            panel1.Invalidate();
+            movePanels();
 
+
+        }
+        private void setPanels()
+        { foreach(Panel p in panels)
+            {
+                p.Width = 2 * hexRadius;
+                p.Height = 2 * hexRadius;
+            }
+        }
+
+        private void movePanels()
+        {
+            PointF hexCenters = new PointF(this.Height / 2 + radius, this.Width / 2);
+            foreach (Panel p in panels)
+            {
+                p.Left = (int)(hexCenters.X - hexRadius);
+                p.Top = (int)(hexCenters.Y - hexRadius);
+                hexCenters = rotate(new PointF(this.Height / 2, this.Width / 2), hexCenters, 60);
+            }
+        }
+
+    }
+    public class TransparentPanel : Panel
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x00000020; // WS_EX_TRANSPARENT
+                return cp;
+            }
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //base.OnPaintBackground(e);
         }
     }
 }
